0ad7dca7fd95296864cdceb4da5c142f3063a8c9
amarix <ammaardeveloper@gmail.com>
2026-07-08 18:30:40 +0700

    Update README.md

--- env.js
+++ env.js
@@ -1,5 +1,4 @@
 import { configurationFileData } from "./modules/see-config.js"
 
 const { configData } = configurationFileData()
-// export const HOST = 'http://backend.local.192.168.18.18.nip.io'
 export const HOST = configData["backend_host"]
